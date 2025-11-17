58930c72f3b823ef5d7ea6091cdc94b0ce8e035d
edocamp98 <edcamp98@gmail.com>
2025-11-17 08:58:35 -0300

    tabuada completa

---
+++ geradortab.js
@@ -0,0 +1,24 @@
+function tabuada(){
+    var txtab = document.getElementById('tab')
+    var seltab = document.getElementById('seltab')
+
+    if (txtab.value.length == 0){
+        window.alert('Digite um número!')
+    } else {
+        var tab = Number(txtab.value)
+        var num = 1
+        seltab.innerHTML=''
+        
+        while (num <= 10){
+            let item = document.createElement('option')
+            item.text = `${tab} x ${num} = ${tab*num}`
+            item.value = `seltab${num}`
+            seltab.appendChild(item)
+            num++
+        }
+
+    }
+    
+
+
+}
