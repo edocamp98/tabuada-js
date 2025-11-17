529762228d54109f4530c1be45ce4c4e11e74bc4
edocamp98 <edcamp98@gmail.com>
2025-11-17 10:26:38 -0300

    event enter adicionado

--- geradortab.js
+++ geradortab.js
@@ -1,29 +1,25 @@
-function clickPress(event){
-    if (event.keyCode == ""){
+var txtab = document.getElementById('tab')
+txtab.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter'){
         tabuada()
     }
-}
-function tabuada(){
-    var txtab = document.getElementById('tab')
-    var seltab = document.getElementById('seltab')
-
-    if (txtab.value.length == 0){
-        window.alert('Digite um número!')
-    } else {
-        var tab = Number(txtab.value)
-        var num = 1
-        seltab.innerHTML=''
-        
-        while (num <= 10){
-            let item = document.createElement('option')
-            item.text = `${tab} x ${num} = ${tab*num}`
-            item.value = `seltab${num}`
-            seltab.appendChild(item)
-            num++
+})
+function tabuada(){        
+        if (txtab.value.length == 0){
+            window.alert('Digite um número!')
+        } else {
+            var tab = Number(txtab.value)
+            var num = 1
+            seltab.innerHTML=''
+            
+            while (num <= 10){
+                let item = document.createElement('option')
+                item.text = `${tab} x ${num} = ${tab*num}`
+                item.value = `seltab${num}`
+                seltab.appendChild(item)
+                num++
+            }
+            
         }
-
     }
     
-
-
-}
